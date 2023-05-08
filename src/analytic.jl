c56252a566e6daa8b55fb5ad06e9d890bcce9416
Steve Flammia <sflammia@gmail.com>
2023-05-08 14:58:33 -0700

    Update analytic.jl

--- src/analytic.jl
+++ src/analytic.jl
@@ -6,6 +6,14 @@ q_pochhammer(a, q, n)
 Finite q-Pochhammer symbol.
 """
 q_pochhammer(a, q, n) = prod([one(a)-a*q^k for k=0:n-1])
+# function q_pochhammer(a, q, n) 
+#     x = one(a)
+#     for k=0:n-1
+#         x *= one(a)-a*q^k
+#     end
+#     x
+# end
+    
 
 
 @doc """
@@ -91,13 +99,63 @@ function ghost(A,d,β,q=[0,0])
 end
 
 
+# function ghost(d,Q::QuadBin,q=[0,0])
+#     D = discriminant(Q)
+    
+#     # test compatability of arguments
+#     t =  coredisc( (d+1)*(d-3) ) .// coredisc(D)
+#     @assert t[1] == 1 "Fundamental discriminant of the quadratic form must equal the fundamental discriminant of Q(√a) where a=(d+1)*(d-3)."
+#     @assert isinteger(t[2]) "Conductor of the quadratic form must divide the conductor of Q(√a) where a=(d+1)*(d-3)."
+    
+#     L = stabilizer(Q)
+#     A = L^sl2zorder(L,d)
+#     β = (-BigFloat(Q.b)+sqrt(BigFloat(D)))/(2Q.a)
+#     ζ = -cispi(BigFloat(1)/d)
+#     QQ = QuadBin(A[2,1],A[2,2]-A[1,1],-A[1,2])
+#     c = e(-BigFloat(rademacher(A))/24) / sqrt(BigFloat(d+1))
+#     W = psl2word(A)
+#     n = length(W)-1
+    
+#     B = zeros(eltype(A),n+2,2)
+#     B[1:2,1:2] = A
+#     for j=1:n
+#         B[j+2,:] = [-1 W[j]]*B[j:j+1,:]
+#     end
+#     ω = BigFloat.(B)*[β; BigFloat(1)]
+#     r = ω ./ circshift(ω,-1)
+
+#     χ = zeros(Complex{BigFloat},d,2)
+#     χ[1,1] = 1
+#     for j = 1:2*d-1
+#         p = radix(j,[d,d])
+#         z = (ω[1]*p[2]-ω[2]*p[1])/d
+#         m = Int((-A[2,1]*p[1]+(A[1,1]-1)*p[2])/d)
+
+#         QA = BigFloat(-QQ(p...)/(d*(d-2)))
+#         s = ( d%2 == 1 ? 1 : (1+p[1])*(1+p[2])+q[1]*p[2]-q[2]*p[1] )
+#         nu = ζ^QA * (-1)^s * c / q_pochhammer_exp( (p[2]*β-p[1])/d, β, m )
+#         for i=1:n
+#             nu *= sigma_S(z/ω[i+2],r[i+1])
+#         end
+    
+#         χ[p[2]+1,p[1]+1] = ζ^(p[2]*p[1])*real(nu)
+#     end
+#     χ = ifft(χ,1)
+#     sqrt(abs(χ[1,1]))*circshift(cumprod(χ[:,2]./χ[:,1]), 1)
+#     # to obtain Ghost projector:
+#     # ψ = ghost(d,q) 
+#     # ϕ = circshift(reverse(ψ),1)
+#     # G = ψ*ϕ'/ϕ'ψ
+# end
+
+
 function ghost(d,Q::QuadBin,q=[0,0])
     D = discriminant(Q)
     
     # test compatability of arguments
     t =  coredisc( (d+1)*(d-3) ) .// coredisc(D)
-    @assert t[1] == 1 "Fundamental discriminant of the quadratic form must equal the fundamental discriminant of Q(√a) where a=(d+1)*(d-3)."
-    @assert isinteger(t[2]) "Conductor of the quadratic form must divide the conductor of Q(√a) where a=(d+1)*(d-3)."
+    @req t[1] == 1 "Fundamental discriminant of the quadratic form must equal the fundamental discriminant of Q(√a) where a=(d+1)*(d-3)."
+    @req isinteger(t[2]) "Conductor of the quadratic form must divide the conductor of Q(√a) where a=(d+1)*(d-3)."
     
     L = stabilizer(Q)
     A = L^sl2zorder(L,d)
@@ -105,17 +163,10 @@ function ghost(d,Q::QuadBin,q=[0,0])
     ζ = -cispi(BigFloat(1)/d)
     QQ = QuadBin(A[2,1],A[2,2]-A[1,1],-A[1,2])
     c = e(-BigFloat(rademacher(A))/24) / sqrt(BigFloat(d+1))
-    W = psl2word(A)
-    n = length(W)-1
-    
-    B = zeros(eltype(A),n+2,2)
-    B[1:2,1:2] = A
-    for j=1:n
-        B[j+2,:] = [-1 W[j]]*B[j:j+1,:]
-    end
-    ω = BigFloat.(B)*[β; BigFloat(1)]
-    r = ω ./ circshift(ω,-1)
 
+    ω = _get_periods(A,β)
+    r = ω ./ circshift(ω,-1)
+    
     χ = zeros(Complex{BigFloat},d,2)
     χ[1,1] = 1
     for j = 1:2*d-1
@@ -126,7 +177,7 @@ function ghost(d,Q::QuadBin,q=[0,0])
         QA = BigFloat(-QQ(p...)/(d*(d-2)))
         s = ( d%2 == 1 ? 1 : (1+p[1])*(1+p[2])+q[1]*p[2]-q[2]*p[1] )
         nu = ζ^QA * (-1)^s * c / q_pochhammer_exp( (p[2]*β-p[1])/d, β, m )
-        for i=1:n
+        for i=1:(length(ω)-2)
             nu *= sigma_S(z/ω[i+2],r[i+1])
         end
     
@@ -139,6 +190,21 @@ function ghost(d,Q::QuadBin,q=[0,0])
     # ϕ = circshift(reverse(ψ),1)
     # G = ψ*ϕ'/ϕ'ψ
 end
+
+
+function _get_periods(A,β)
+    W = psl2word(A)
+    n = length(W)-1
+    
+    B = zeros(eltype(A),n+2,2)
+    B[1:2,1:2] = A
+    for j=1:n
+        B[j+2,:] = [-1 W[j]]*B[j:j+1,:]
+    end
+    BigFloat.(B)*[β; BigFloat(1)]
+end
+
+
 
 
 function sigma_S(z,β)
