8cc1473ae49387b767e7f7c59441617087051ccf
Steve Flammia <sflammia@gmail.com>
2024-06-12 09:33:02 -0400

    Update integer_relations.jl

--- src/integer_relations.jl
+++ src/integer_relations.jl
@@ -1,51 +1,27 @@
+export guess_int_null_vec
+
 @doc """
-    guess_int_null_vec( x::Vector{BigFloat} [; warn::Bool = false])
+    guess_int_null_vec( x::Vector{BigFloat})
 
 Use LLL to find an integer relation among the elements of `x`.
+
+# Examples
+
+'''
+julia> guess_int_null_vec(BigFloat.([1; sin(big(pi)/8)^2; sin(big(pi)/4)]))
+3-element Vector{BigInt}:
+ -1
+  2
+  1
+'''
 """
-function guess_int_null_vec( x::Vector{BigFloat}; warn::Bool = false)
+function guess_int_null_vec( x::Vector{BigFloat})
     prec = precision(x[1])
     t = ZZ(2)^prec
     n = length(x)
-    # maxnorm = 2maximum(ZZ.(ceil.(x)))*t+1
     v = ZZ.(round.(t .* x))
     L = matrix_space(ZZ,n,n+1)
     T = L([ ZZ(j==k) + (k==n+1)*v[j] for j=1:n, k=1:n+1])
-    # B = dropdims(BigInt.(lll_with_removal(T,maxnorm)[2][1,:]),dims=1)[1:n]
-    # B = BigInt.(lll_with_removal(T,maxnorm)[2][1,:])[1:n]
-    # println(lll(T)[1,1:end-1])
-    B = BigInt.(lll(T)[1,:])[1:n]
 
-    if warn && -log2( abs( dot(x,B) ) ) - prec + 2n < 0
-        @warn "Precision of dot product is low; integer relation may be spurious."
-    end
-
-    return B
-end
-
-
-
-@doc """
-    round_conj( F::AdmissibleTuple, V::Vector{BigFloat})
-
-Round the elements of `V` into the class field `F.H` and then Galois conjugate with the sign-switching automorphism `F.g`.
-This uses an integer relation heuristic for the rounding step, and if this fails to detect the "true" exact value then the output is unpredictable.
-"""
-function round_conj( F::AdmissibleTuple, V::Vector{BigFloat})
-    hb = lll(maximal_order(F.H)).basis_nf # find a good basis
-    eH = real_embeddings(F.H)[1] # need to pick a real embedding
-    prec = precision(V[1])
-    fH = x -> BigFloat.(real.(evaluation_function( eH, prec).(x)))
-    primalbasis = fH.(hb)
-    dualbasis = fH.(F.g.(hb))
-    W = copy(V)
-
-    k = 1
-    for v in V
-        t = guess_int_null_vec( [ primalbasis; v] )
-        x = -t[1:end-1]/t[end]
-        W[k] = dot( dualbasis, x )
-        k += 1
-    end
-    W
+    return BigInt.(lll(T)[1,:])[1:n]
 end
