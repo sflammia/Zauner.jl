d19601337f8b2636f0173c6acef33992822c3d91
Steve Flammia <sflammia@gmail.com>
2024-07-09 13:48:37 -0400

    Update Zauner.jl

--- src/Zauner.jl
+++ src/Zauner.jl
@@ -14,8 +14,8 @@ using SCS
 import Hecke.conductor, Hecke.cycle, Hecke.reduction, Hecke.stabilizer
 export conductor, cycle, reduction, stabilizer
 
-include("utils.jl")
 include("dQ.jl")
+include("utils.jl")
 include("algebraic.jl")
 include("hj.jl")
 include("analytic.jl")
