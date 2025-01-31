17407c4b844793292492fe037a3a1333a52016c4
Steve Flammia <sflammia@gmail.com>
2025-01-31 08:03:05 -0500

    Use const sl2z generators instead of functions

--- src/sl2z_arithmetic.jl
+++ src/sl2z_arithmetic.jl
@@ -3,26 +3,18 @@
 
 export matrix_I, matrix_P, matrix_S, matrix_T, sl2z_inverse, sl2z_act
 
-function matrix_I()
-    [ZZ(1) ZZ(0); ZZ(0) ZZ(1)]
-end
+const matrix_I = [ZZ(1) ZZ(0); ZZ(0) ZZ(1)]
 
-function matrix_P()
-    [-ZZ(1) ZZ(0); ZZ(0) -ZZ(1)]
-end
+const matrix_P = [-ZZ(1) ZZ(0); ZZ(0) -ZZ(1)]
 
-function matrix_S()
-    [ZZ(0) -ZZ(1); ZZ(1) ZZ(0)]
-end
+const matrix_S = [ZZ(0) -ZZ(1); ZZ(1) ZZ(0)]
 
-function matrix_T()
-    [ZZ(1) ZZ(1); ZZ(0) ZZ(1)]
-end
+const matrix_T = [ZZ(1) ZZ(1); ZZ(0) ZZ(1)]
 
 function sl2z_inverse(A::Matrix)
-    [A[2,2] -A[1,2]; -A[2,1] A[1,1]]
+    [A[2, 2] -A[1, 2]; -A[2, 1] A[1, 1]]
 end
 
 function sl2z_act(A::Matrix, x)
-    (BigInt(A[1,1])*x+BigInt(A[1,2]))/(BigInt(A[2,1])*x+BigInt(A[2,2]))
+    (A[1, 1] * x + A[1, 2]) / (A[2, 1] * x + A[2, 2])
 end
