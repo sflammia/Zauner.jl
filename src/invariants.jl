1c716eedfb9a9b5784266e81491ad132a74eb2b5
Gene Kopp <kopp@math.lsu.edu>
2025-10-17 13:46:30 -0500

    Still debugging invariants.jl

--- src/invariants.jl
+++ src/invariants.jl
@@ -182,7 +182,7 @@ function necromancy(F::AdmissibleTuple;
             # otherwise break and try again
             for ol in L[j]
                 # complex_phases &= all(abs.(abs.(L[j]) - 1) < 10^(-10))
-                complex_phases &= abs.(abs.ol - 1) < 10^(-10)
+                complex_phases &= abs(abs(ol) - 1) < 10^(-10)
             end
             !complex_phases && break
         end
