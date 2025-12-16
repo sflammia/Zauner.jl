c5385ba3bd343fc6ce5c0c7e4dc1f0b2db22eec8
Steve Flammia <sflammia@gmail.com>
2025-12-16 03:14:30 -0500

    Update invariants.jl
    major update.

--- src/invariants.jl
+++ src/invariants.jl
@@ -1,46 +1,145 @@
 export necromancy, pseudonecromancy, ghost_orbit, class_field_bases
 
-# struct ApproximateNormalBasis
-#     x::Vector{T}          # normal basis
-#     p::Vector{Complex{T}} # fourier basis
-#     y::Vector{T}          # dual basis
-# end
 
-# function ApproximateNormalBasis(x::Vector{T})
-#     p = ifft(x)
-#     y = real.(ifft(1 ./ p))/length(x)
-#     ApproximateNormalBasis(x, p, y)
-# end
-
-
-function ghost_orbit(F::AdmissibleTuple, prec::Integer;
-    base::Integer=10,
-    verbose::Bool=false)
-
-    verbose && println(F)
-
-    # the normal form orders of the Galois group and a maximal p-orbit
+function ghost_orbit(
+    F::AdmissibleTuple,
+    ψ::Vector{Complex{BigFloat}}
+)
     ords, porb = galois_order_orbit(F)
-    verbose && println("Galois group with orders ", ords)
 
-    # get a low-precision ghost
-    INIT_PREC = 64
-    verbose && println("Computing the initial ghost to low $(INIT_PREC) bits.")
-    setprecision(BigFloat, INIT_PREC; base=2)
-    verbose ? (@time ψ = ghost(F)) : ψ = ghost(F)
+    prec = precision(real(ψ[1]))
+    setprecision(BigFloat, prec) do
+        ϕ = circshift(reverse(ψ), 1)
+        ϕ .*= BigFloat(F.d + 1) / (ϕ'ψ)
+        K = [real(ϕ' * wh(p, ψ)) for p in porb]
+        return reshape(K, ords...)
+    end
+end
 
-    verbose && println("Precision of initial ghost is ", precision(real(ψ[1])), " bits")
 
-    ψ = precision_bump(ψ, prec; base=base, verbose=verbose)
-    ϕ = circshift(reverse(ψ), 1)
-    ϕ .*= (BigFloat(F.d + 1)) / ϕ'ψ # include normalization factors
-    verbose && println("Ghost precision is now ", precision(real(ψ[1])), " bits")
+function _ensure_ghost!(
+    F::AdmissibleTuple,
+    ψ::Union{Nothing,Vector{Complex{BigFloat}}},
+    target_prec::Int;
+    base=10,
+    verbose=false
+)
+    if ψ === nothing
+        verbose && println("Computing an initial ghost at low precision.")
+        # initial precision is set to 64 bits
+        setprecision(BigFloat, 64) do
+            ψ = ghost(F)
+        end
+    end
 
-    # compute the ghost overlaps
-    verbose && println("Computing the high-precision ghost overlaps.")
-    (verbose ? (@time K = [real(ϕ'wh(p, ψ)) for p in porb]) : K = [real(ϕ'wh(p, ψ)) for p in porb])
+    ψ = precision_bump(ψ, target_prec; base=base, verbose=verbose)
+    return ψ
+end
 
-    return K
+
+# Inner loop version
+function pseudonecromancy(
+    F::AdmissibleTuple,
+    prec::Int;
+    ghost_cache::Union{Nothing,Vector{Complex{BigFloat}}}=nothing,
+    recipes::Union{Nothing,Vector{NormalBasisRecipe}}=nothing,
+    verbose=false,
+    base=10 # possible inconsistency in default base
+)
+
+    Ψ_cache = _ensure_ghost!(F, ghost_cache, prec; verbose=verbose, base=base)
+    u = ghost_orbit(F, Ψ_cache)
+    verbose && println("Precisions of ghost orbit: $(precision(u[1])) and precision of Ψ_cache = $(precision(real(Ψ_cache[1])))")
+
+    setprecision(precision(u[1])) do
+        if recipes === nothing
+            x, p, y, recipes = ghost_basis(u; verbose=verbose)
+        else
+            x, p, y, _ = ghost_basis(u; recipes=recipes, verbose=verbose)
+        end
+        verbose && println("Precisions of ghost basis: $(precision(x[1][1])) and $(precision(y[1][1]))")
+        A = Zauner._multiplication_matrix(x, p, y)
+        szu = size(u)
+
+        # sign-switch A and construct z
+        # Note: `prec` should be in bits here.
+        Hprimal, Hdual = class_field_bases(F, prec)
+
+        z = map(x -> similar(x, ComplexF64), p)
+        for i = 1:length(z)
+            B = Matrix{Float64}(undef, size(A[i])...)
+            for t in CartesianIndices(A[i])
+                B[t] = Float64(_dualize(Hprimal, Hdual, A[i][t], prec))
+            end
+            vals, vecs = eigen(B)
+            z[i] = (vals[1] / vecs[1, 1]) .* vecs[:, 1]
+        end
+
+        Y = (length(y) == 1 ? y[1] : kron(reverse(y)...))
+        s = zeros(Float64, szu)
+
+        # This is a convolution and could be optimized by using FFTs
+        for t = 0:length(u)-1
+            shift = radix(t, szu)
+            c = dot(circshift(u, shift)[:], Y)
+
+            if log10.(abs.(c)) .< -prec
+                continue # s is zero, no need to dualize
+            end
+            s[(1 .+ shift)...] = Float64(_dualize(Hprimal, Hdual, c, prec))
+        end
+
+        # This could be optimized by using FFTs
+        Z = (length(z) == 1 ? z[1] : kron(reverse(z)...))
+        uu = zeros(ComplexF64, szu)
+        for t = 0:length(u)-1
+            shift = radix(t, szu)
+            uu[(1 .+ shift)...] = dot(circshift(s, shift)[:], Z)
+        end
+
+        @assert all(abs2.(uu) .≈ F.d + 1) "Reconstructed SIC units are not proportional to complex phase units, √(d+1)×exp(iθ)."
+
+        return uu, recipes, Ψ_cache
+    end
+end
+
+
+function pseudonecromancy(
+    F::AdmissibleTuple;
+    init_prec::Integer=2^8,
+    max_prec::Integer=2^14,
+    base=10,
+    verbose=false,
+)
+
+    basename = _base_name(base)
+    if init_prec < log(base, 10) * 2^6
+        verbose && @info "Increasing initial precision from $init_prec $basename to $(2^6) digits."
+    end
+    prec = max(init_prec, ceil(Int, log(base, 10) * 2^6))
+
+    ψ_cache = nothing
+    recipes = nothing
+
+    verbose && "Starting pseudonecromancy computation with initial precision $prec $basename."
+    while prec ≤ max_prec
+        try
+            u, recipes, ψ_cache = pseudonecromancy(
+                F,
+                prec;
+                ghost_cache=ψ_cache,
+                recipes=recipes,
+                verbose=verbose,
+                base=base
+            )
+            return u
+        catch err
+            verbose && @warn "Failed to compute pseudonecromancy for precision $prec, with error:\n$(err)\n Doubling precision and trying again."
+        end
+        prec *= 2
+    end
+
+    error("Failed to achieve pseudonecromancy with $max_prec $basename. Try increasing `max_prec`.")
 end
 
 
@@ -81,7 +180,7 @@ end
 
 
 
-function class_field_bases(F::AdmissibleTuple, prec::Int)
+function class_field_bases(F::AdmissibleTuple, bits::Integer)
     # Make sure that the class field and sign-switching automorphism are initialized
     ghostclassfield(F)
     signswitch(F)
@@ -91,111 +190,12 @@ function class_field_bases(F::AdmissibleTuple, prec::Int)
     eH = real_embeddings(F.H)[1] # fix a real embedding
 
     # Evaluate at the embedding eH.
-    _fH(x) = BigFloat.(real.(evaluation_function(eH, prec).(x)))
+    _fH(x) = BigFloat.(real.(evaluation_function(eH, bits).(x)))
     primalbasis = _fH.(hb)
     dualbasis = _fH.(gb)
 
-    # return numerical bases to precision prec.
+    # return numerical bases to precision bits.
     return primalbasis, dualbasis
-end
-
-
-
-function pseudonecromancy(F::AdmissibleTuple, prec::Int; recipes::Union{Nothing,Vector{NormalBasisRecipe}}=nothing, verbose=false, base=10)
-    u = ghost_orbit(F, prec; verbose=verbose, base=base)
-    if recipes === nothing
-        x, p, y, recipes = ghost_basis(u; verbose=verbose)
-    else
-        x, p, y, _ = ghost_basis(u; recipes=recipes, verbose=verbose)
-    end
-
-    A = Zauner._multiplication_matrix(x, p, y)
-    szu = size(u)
-
-    # sign-switch A and construct z
-    Hprimal, Hdual = class_field_bases(F, prec)
-
-    z = similar.(p)
-    for i = 1:length(z)
-        B = Matrix{Float64}(undef, size(A[i])...)
-        for t in CartesianIndices(A[i])
-            # println("t = $t")
-            R = guess_int_null_vec([Hprimal; A[i][t]], 3 * prec ÷ 4)
-            # println(R)
-            R2 = guess_int_null_vec([Hprimal; A[i][t]], prec)
-            # println(R2)
-            if R == R2 || R == -R2
-                B[t] = Float64(-dot(Hdual, R[1:end-1]) / R[end])
-            else
-                error("Unreliable sign-switch of A matrix.")
-            end
-        end
-        vals, vecs = eigen(B)
-        z[i] = (vals[1] / vecs[1, 1]) .* vecs[:, 1]
-    end
-
-    Y = (length(y) == 1 ? y[1] : kron(reverse(y)...))
-    s = zeros(Float64, szu)
-
-    # This is a convolution and could be optimized by using FFTs
-    for t = 0:length(u)-1
-        shift = radix(t, szu)
-        c = dot(circshift(u, shift)[:], Y)
-
-        if log10.(abs.(c)) .< -prec
-            continue
-        end
-        R = guess_int_null_vec([Hprimal; c], 3 * prec ÷ 4)
-        R2 = guess_int_null_vec([Hprimal; c], prec)
-
-        if R == R2 || R == -R2
-            s[(1 .+ shift)...] = Float64((Hdual' * R[1:end-1]) / (-R[end]))
-        else
-            error("Unreliable sign-switch of c vector.")
-        end
-    end
-
-    # This could be optimized by using FFTs
-    Z = ComplexF64.(length(z) == 1 ? z[1] : kron(reverse(z)...))
-    uu = zeros(ComplexF64, szu)
-    for t = 0:length(u)-1
-        shift = radix(t, szu)
-        uu[(1 .+ shift)...] = dot(circshift(s, shift)[:], Z)
-    end
-
-    @assert all(abs2.(uu) .≈ F.d + 1) "Reconstructed SIC units are not proportional to complex phase units, √(d+1)×exp(iθ)."
-
-    return uu, recipes
-end
-
-function pseudonecromancy(
-    F::AdmissibleTuple;
-    init_prec::Integer=2^8,
-    max_prec::Integer=2^14,
-    base=10,
-    verbose=false,
-)
-
-    basename = _base_name(base)
-    if init_prec < log(base, 10) * 2^6
-        verbose && @info "Increasing initial precision from $init_prec $basename to $(2^6) digits."
-    end
-    prec = max(init_prec, round(Integer, log(base, 10) * 2^6, RoundUp))
-
-    recipes = nothing
-
-    verbose && @info "Starting pseudonecromancy computation with initial precision $prec $basename."
-    while prec ≤ max_prec
-        try
-            u, recipes = pseudonecromancy(F, prec; recipes=recipes, verbose=verbose, base=base)
-            return u
-        catch err
-            verbose && @warn "Failed to compute pseudonecromancy for precision $prec, with error:\n$(err)\n Doubling precision and trying again."
-        end
-        prec *= 2
-    end
-
-    error("Failed to achieve pseudonecromancy with $max_prec $basename. Try increasing `max_prec`.")
 end
 
 
@@ -229,16 +229,6 @@ function necromancy(F::AdmissibleTuple;
     @warn "This function is being updated and is presently nonfunctional."
     return nothing
 
-    # Now try every shift in the Galois group until one of them gives a SIC
-    verbose && println("Now searching through Galois shifts and using matrix completion.")
-    for k = 0:n-1
-        ψ = matrix_completion(circshift(x, radix(k, ords)), F)
-        sft = sic_frame_test(ψ)
-        if sft < overlap_tol
-            verbose && println("Fiducial vector found with frame equations correct to ≤ $sft.")
-            break
-        end
-    end
 
     # Finally, improve the precision again to standard BigFloat.
     z = re_im_proj(Complex{BigFloat}.(ψ))
@@ -255,14 +245,19 @@ end
 
 
 @doc """
-    _dualize( primal::Vector{T}, dual::Vector{T}, x::T) where T::AbstractFloat
+    _dualize( primal::Vector{T}, dual::Vector{T}, x::T, prec::Integer) where T::AbstractFloat
 
 Internal function that takes an `AbstractFloat` number `x`, rounds it into the `primal` basis, then expands it again in the `dual` basis.
-If `x` is not faithfully represented in the `primal` basis then the result is unpredictable.
+If `x` is not reliably reconstructed in the `primal` basis at `prec` and `3 * prec ÷ 4`, then it throws an error.
 If `primal` and `dual` are related by a galois automorphism `g`, then ideally this outputs an approximation of `g(x)`.
 """
-function _dualize(primal::Vector{T}, dual::Vector{T}, x::T) where {T<:AbstractFloat}
-    t = guess_int_null_vec([primal; x])
-    # println("‖t‖₁ = ", sum(abs.(t)))
-    return -dot(dual, t[1:end-1]) / t[end]
+function _dualize(primal::Vector{T}, dual::Vector{T}, x::T, prec::Integer) where {T<:AbstractFloat}
+    R = guess_int_null_vec([primal; x], 3 * prec ÷ 4)
+    R2 = guess_int_null_vec([primal; x], prec)
+
+    if R == R2 || R == -R2
+        return (dual' * R[1:end-1]) / (-R[end])
+    else
+        error("Unreliable integer relations: dualization failed.")
+    end
 end
